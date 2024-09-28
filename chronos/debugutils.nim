ba4e80f9d6be26280d7135ac50776f03699d0ace
Ivan Folgueira Bande <ivansete@status.im>
2024-09-28 16:45:34 +0200

    temporary change to allow future track without changing nwaku ci

--- chronos/debugutils.nim
+++ chronos/debugutils.nim
@@ -12,14 +12,14 @@
 import "."/[asyncloop, config]
 export asyncloop
 
-when chronosFutureTracking:
-  import stew/base10
+import stew/base10
 
 const
-  AllFutureStates* = {FutureState.Pending, FutureState.Cancelled,
-                      FutureState.Completed, FutureState.Failed}
-  WithoutCompleted* = {FutureState.Pending, FutureState.Cancelled,
-                      FutureState.Failed}
+  AllFutureStates* = {
+    FutureState.Pending, FutureState.Cancelled, FutureState.Completed,
+    FutureState.Failed,
+  }
+  WithoutCompleted* = {FutureState.Pending, FutureState.Cancelled, FutureState.Failed}
   OnlyPending* = {FutureState.Pending}
   OnlyCompleted* = {FutureState.Completed}
 
@@ -31,27 +31,25 @@ proc dumpPendingFutures*(filter = AllFutureStates): string =
   ##    not yet finished).
   ## 2. Future[T] objects with ``FutureState.Finished/Cancelled/Failed`` state
   ##    which callbacks are scheduled, but not yet fully processed.
-  when chronosFutureTracking:
-    var count = 0'u
-    var res = ""
-    for item in pendingFutures():
-      if item.state in filter:
-        inc(count)
-        let loc = item.location[LocationKind.Create][]
-        let procedure = $loc.procedure
-        let filename = $loc.file
-        let procname = if len(procedure) == 0:
+  var count = 0'u
+  var res = ""
+  for item in pendingFutures():
+    if item.state in filter:
+      inc(count)
+      let loc = item.location[LocationKind.Create][]
+      let procedure = $loc.procedure
+      let filename = $loc.file
+      let procname =
+        if len(procedure) == 0:
           "\"unspecified\""
         else:
           "\"" & procedure & "\""
-        let item = "Future[" & Base10.toString(item.id) & "] with name " &
-                   $procname & " created at " & "<" & filename & ":" &
-                   Base10.toString(uint(loc.line)) & ">" &
-                   " and state = " & $item.state & "\n"
-        res.add(item)
-    Base10.toString(count) & " pending Future[T] objects found:\n" & $res
-  else:
-    "0 pending Future[T] objects found\n"
+      let item =
+        "Future[" & Base10.toString(item.id) & "] with name " & $procname &
+        " created at " & "<" & filename & ":" & Base10.toString(uint(loc.line)) & ">" &
+        " and state = " & $item.state & "\n"
+      res.add(item)
+  Base10.toString(count) & " pending Future[T] objects found:\n" & $res
 
 proc pendingFuturesCount*(filter: set[FutureState]): uint =
   ## Returns number of `pending` Future[T] objects which satisfy the ``filter``
@@ -59,14 +57,11 @@ proc pendingFuturesCount*(filter: set[FutureState]): uint =
   ##
   ## If ``filter`` is equal to ``AllFutureStates`` Operation's complexity is
   ## O(1), otherwise operation's complexity is O(n).
-  when chronosFutureTracking:
-    if filter == AllFutureStates:
-      pendingFuturesCount()
-    else:
-      var res = 0'u
-      for item in pendingFutures():
-        if item.state in filter:
-          inc(res)
-      res
+  if filter == AllFutureStates:
+    pendingFuturesCount()
   else:
-    0'u
+    var res = 0'u
+    for item in pendingFutures():
+      if item.state in filter:
+        inc(res)
+    res
